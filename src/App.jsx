8b27f66898861af4ba9ed6e1ae2952358da558a8
Shreyaa Sharma <shreyasharma.ss305@gmail.com>
2023-01-11 01:05:12 +0530

    Todo list with local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,102 +1,70 @@
 import React from "react";
 import './App.css';
+import { useState, useEffect } from "react";
 
-class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    };
+const App = () => {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+		const todos = JSON.parse(localStorage.getItem('todos'));
+		if (todos) {
+			setTodos(todos);
+		}
+	}, []);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const addItem = () => {
+    if (todo != "") {
+      setTodos([...todos, todo]);
+      setTodo("");
+    }
   }
 
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext,
+  const deleteItem = (text) => {
+    const newTodos = todos.filter((todo) => {
+      return todo != text;
     });
+    setTodos(newTodos);
   }
 
-  renderSquare(i) {
-    return (
-      <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
-      />
-    );
-  }
-
-  render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
-
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-}
-
-function Square(props) {
   return (
-    <button className="square" onClick={props.onClick}>
-      {props.value}
-    </button>
+    <div className="App">
+      <h1> React Todo App</h1>
+
+      <div className="input-wrapper">
+        <input
+          type="text"
+          name="todo"
+          value={todo}
+          placeholder="Create a new todo"
+          onChange={(e) => {
+            setTodo(e.target.value)
+          }}
+        />
+        <button className="add-button" onClick={addItem}>Add</button>
+
+        {todos.length > 0 ? (
+          <ul className="todo-list">
+            {todos.map((todo, index) => (
+              <div className="todo">
+                <li key={index}> {todo} </li>
+                <button className="delete-button" onClick={() => { deleteItem(todo) }}>Delete</button>
+              </div>
+            ))}
+          </ul>
+        ) : (
+          <div className="empty">
+            <p>No task</p>
+          </div>
+        )}
+
+      </div>
+    </div>
   )
 }
 
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
-
-export default () => (
-  <>
-    <div className="game">
-      <div className="game-board">
-        <Board />
-      </div>
-    </div>
-  </>
-);
+export default App;
